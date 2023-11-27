049ff8041883b8dc2d4e997a3a0aeab65e99cd8a
Maddiew03 <madisson.whelpley@uleth.ca>
2023-11-27 14:01:54 -0700

    6th

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
- 
+var myImage = document.querySelector("#imageid");
+
+function chloe() {
+    myImage.src = "images/fairy.png";
+
+}
+function princess() {
+    myImage.src = "images/princess.png";
+
+}
+function detective() {
+    myImage.src = "images/detective.png";
+
+}
+function angel() {
+    myImage.src = "images/angel.png";
+
+}
+function fairy() {
+    myImage.src = "images/chloe.png";
+
+}
